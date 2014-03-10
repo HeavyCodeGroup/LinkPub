203b1e5fc50508ef09f8b43fa9e870a03d8d6176
Yaroslav Osypenko <harentius@yandex.ru>
2014-03-10 18:24:52 +0200

    Menu development

--- src/HeavyCodeGroup/LinkPub/GuiBundle/Controller/ClientController.php
+++ src/HeavyCodeGroup/LinkPub/GuiBundle/Controller/ClientController.php
@@ -8,21 +8,21 @@ class ClientController extends Controller
 {
     public function dashboardAction()
     {
-        return $this->render('@LinkPubGui/Client/dashboard.html.twig');
+        return $this->render('LinkPubGuiBundle:Client:dashboard.html.twig');
     }
 
     public function sitesAction()
     {
-        return $this->render('@LinkPubGui/Client/sites.html.twig');
+        return $this->render('LinkPubGuiBundle:Client:sites.html.twig');
     }
 
     public function incomingLinksAction()
     {
-        return $this->render('@LinkPubGui/Client/incomingLinks.html.twig');
+        return $this->render('LinkPubGuiBundle:Client:incomingLinks.html.twig');
     }
 
     public function outgoingLinksAction()
     {
-        return $this->render('@LinkPubGui/Client/outgoingLinks.html.twig');
+        return $this->render('LinkPubGuiBundle:Client:outgoingLinks.html.twig');
     }
 }

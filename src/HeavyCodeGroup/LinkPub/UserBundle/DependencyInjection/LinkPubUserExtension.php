0e5a716acc9f172f5bcfa485fe88a870637ef7a1
vadim <vadim_2991@ukr.net>
2014-04-17 18:17:32 +0300

    add vk and add facebook

--- src/HeavyCodeGroup/LinkPub/UserBundle/DependencyInjection/LinkPubUserExtension.php
+++ src/HeavyCodeGroup/LinkPub/UserBundle/DependencyInjection/LinkPubUserExtension.php
@@ -15,8 +15,6 @@ class LinkPubUserExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }

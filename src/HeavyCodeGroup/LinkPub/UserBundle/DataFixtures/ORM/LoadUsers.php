8b2bf3d747fda328eb4d6db654409cf37c5735e6
vadim <vadim_2991@ukr.net>
2014-04-19 00:05:40 +0300

    add fixtures page

--- src/HeavyCodeGroup/LinkPub/UserBundle/DataFixtures/ORM/LoadUsers.php
+++ src/HeavyCodeGroup/LinkPub/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -6,6 +6,7 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use HeavyCodeGroup\LinkPub\UserBundle\Entity\User;
+use Symfony\Component\Yaml\Yaml;
 
 class LoadUsers extends AbstractFixture implements OrderedFixtureInterface
 {
@@ -16,26 +17,44 @@ class LoadUsers extends AbstractFixture implements OrderedFixtureInterface
      */
     function load(ObjectManager $manager)
     {
-        $admin = new User();
-        $admin->setUsername('admin');
-        $admin->setEmail('admin@mail.net');
-        $admin->setPlainPassword('admin');
-        $admin->setEnabled(true);
-        $admin->setSuperAdmin(true);
-        $manager->persist($admin);
 
-        $user1 = new User();
-        $user1->setUsername('user1');
-        $user1->setEmail('user1@mail.net');
-        $user1->setPlainPassword('user1');
-        $user1->setEnabled(true);
-        $user1->setSuperAdmin(false);
-        $manager->persist($user1);
+        $data = $this->getData();
 
-        $manager->flush();
+        foreach ($data as $userData) {
+            $user = new User();
 
-        $this->setReference('user_admin', $admin);
-        $this->setReference('user_user1', $user1);
+            $user->setUsername($userData['username']);
+            $user->setEmail($userData['email']);
+            $user->setPlainPassword($userData['plainPassword']);
+            $user->setEnabled($userData['enabled']);
+            $user->setSuperAdmin($userData['superAdmin']);
+            $manager->persist($user);
+            $manager->flush();
+            $this->setReference($userData['reference'], $user);
+        }
+
+//        $manager->flush();
+//
+//        $admin = new User();
+//        $admin->setUsername('admin');
+//        $admin->setEmail('admin@mail.net');
+//        $admin->setPlainPassword('admin');
+//        $admin->setEnabled(true);
+//        $admin->setSuperAdmin(true);
+//        $manager->persist($admin);
+//
+//        $user1 = new User();
+//        $user1->setUsername('user1');
+//        $user1->setEmail('user1@mail.net');
+//        $user1->setPlainPassword('user1');
+//        $user1->setEnabled(true);
+//        $user1->setSuperAdmin(false);
+//        $manager->persist($user1);
+//
+//        $manager->flush();
+//
+//        $this->setReference('user_admin', $admin);
+//        $this->setReference('user_user1', $user1);
     }
 
     /**
@@ -46,5 +65,10 @@ class LoadUsers extends AbstractFixture implements OrderedFixtureInterface
     function getOrder()
     {
         return 10;
+    }
+
+    protected function getData()
+    {
+        return Yaml::parse(__DIR__ . '/data/user.yml');
     }
 }

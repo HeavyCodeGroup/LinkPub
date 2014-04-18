d93f4d5cc96c8f36389851fa167389e08c0a0f63
vadim <vadim_2991@ukr.net>
2014-04-19 00:29:39 +0300

    add index pehe

--- src/HeavyCodeGroup/LinkPub/StorageBundle/DataFixtures/ORM/LoadPage.php
+++ src/HeavyCodeGroup/LinkPub/StorageBundle/DataFixtures/ORM/LoadPage.php
@@ -9,7 +9,7 @@ use HeavyCodeGroup\LinkPub\StorageBundle\Entity\Page;
 use HeavyCodeGroup\LinkPub\StorageBundle\Entity\Site;
 use Symfony\Component\Yaml\Yaml;
 
-class LoadSites extends AbstractFixture implements OrderedFixtureInterface
+class LoadPage extends AbstractFixture implements OrderedFixtureInterface
 {
     /**
      * Load data fixtures with the passed EntityManager
@@ -20,19 +20,22 @@ class LoadSites extends AbstractFixture implements OrderedFixtureInterface
     {
         $data = $this->getData();
 
-        foreach ($data as $siteData) {
-            $site = new Site();
-            $site->setCategory($this->getReference('category_' . $siteData['category']));
-            $site->setTitle($siteData['title']);
-            $site->setRootUrl($siteData['url']);
-            $site->setOwner($this->getReference($siteData['reference']));
-            $manager->persist($site);
-            $this->addReference($siteData['referenceSite'], $site);
+        foreach ($data as $pageData) {
+            $page = new Page();
+            $page->setUrl($pageData['url']);
+            $page->setLevel($pageData['level']);
+            $page->setPrice($pageData['price']);
+            $page->setCapacity($pageData['capacity']);
+            $page->setPageRank($pageData['page_rank']);
+            $page->setState($pageData['state']);
+
+            $page->setSite($this->getReference($pageData['referenceSite']));
+            $manager->persist($page);
+
             $manager->flush();
         }
 
     }
-
     /**
      * Get the order of this fixture
      *
@@ -40,11 +43,11 @@ class LoadSites extends AbstractFixture implements OrderedFixtureInterface
      */
     function getOrder()
     {
-        return 21;
+        return 23;
     }
 
     protected function getData()
     {
-        return Yaml::parse(__DIR__ . '/data/sites.yml');
+        return Yaml::parse(__DIR__ . '/data/page.yml');
     }
 }
